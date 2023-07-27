f6104f41c420b7a83f5da1ac155f27610f7de1c1
Aidan <aidan.marinelarena@versia.com>
2023-07-27 15:02:22 +0200

    Añadidos Test

---
+++ CleanArchitecture/ArchitectureNUnitTest/DomainNUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchitecture.Domain.Entities;
+using NetArchTest.Rules;
+using System.Reflection;
+
+namespace ArchitectureNUnitTest
+{
+    public class DomainNUnitTest
+    {
+        private static Assembly DomainAssembly => typeof(User).Module.Assembly;
+        [Test]
+        public void Test1()
+        {
+            var types= Types.InAssembly(DomainAssembly);
+            var result= types.That().AreClasses().Should().BeSealed().Or().BeAbstract().GetResult();
+            string fallos = GetFailingTypes(result);
+            Assert.That(result.IsSuccessful, Is.True);
+            //Assert.That(testResult.IsSuccessful, Is.True);
+        }
+        private string GetFailingTypes(TestResult result)
+        {
+            return result.FailingTypeNames != null ?
+                string.Join(", ", result.FailingTypeNames) :
+                string.Empty;
+        }
+    }
+}
